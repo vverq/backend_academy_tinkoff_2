7d3816658db59b5e17a9567b1b903affcb62910c
vverq <vverq00@gmail.com>
2023-03-28 23:04:02 +0500

    update requirments

--- backend/app/alembic/env.py
+++ backend/app/alembic/env.py
@@ -6,11 +6,13 @@ from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
+from app.database import Base
+
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-
+revision_environment=True
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
@@ -18,10 +20,8 @@ if config.config_file_name is not None:
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-import database
-
 # target_metadata = mymodel.Base.metadata
-target_metadata = database.Base
+target_metadata = Base.metadata
 
 
 # other values from the config, defined by the needs of env.py,
@@ -31,7 +31,7 @@ target_metadata = database.Base
 
 
 def run_migrations_offline() -> None:
-    """Run migrations in 'offline' mode.
+    """Run alembic in 'offline' mode.
 
     This configures the context with just a URL
     and not an Engine, though an Engine is acceptable
@@ -80,7 +80,7 @@ async def run_async_migrations() -> None:
 
 
 def run_migrations_online() -> None:
-    """Run migrations in 'online' mode."""
+    """Run alembic in 'online' mode."""
 
     asyncio.run(run_async_migrations())
 
---
+++ backend/app/alembic/versions/9c4f40f9f878_init.py
@@ -0,0 +1,24 @@
+"""init
+
+Revision ID: 9c4f40f9f878
+Revises: 
+Create Date: 2023-03-28 22:37:34.808005
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '9c4f40f9f878'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
